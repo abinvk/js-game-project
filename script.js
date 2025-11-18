840088b6db71c55382c18b1fbf360b37b4994937
Abin vk <vettikkadanabin@gmail.com>
2025-11-18 20:45:09 +0530

    game

--- script.js
+++ script.js
@@ -12,3 +12,18 @@ document.addEventListener('keypress',() => {
     if (!dino.classList.contains('jump-animation'))
     jump();
 });
+setInterval(() => {
+    const dinoTop = parseInt(window.getComputedStyle(dino)
+    .getPropertyValue('top'));
+    const rockLeft = parseInt(window.getComputedStyle(rock)
+    .getPropertyValue('left'));
+
+    if (rockLeft < 0){
+        rock.style.display = 'none';
+    } else {
+        rock.style.disply = '';
+    }
+
+    if(rockLeft < 50 && rockLeft > 0 && dinoTop >  150 )
+
+    }, 50);       
